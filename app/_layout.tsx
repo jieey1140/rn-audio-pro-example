2d4554e953a6382c911df3730fb8a3d0661c94e2
sarasinakei <seihoonkim@redredgroup.com>
2025-04-22 16:36:35 +0900

    commit

---
+++ app/_layout.tsx
@@ -0,0 +1,55 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { AudioPro, AudioProContentType } from 'react-native-audio-pro'
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+    // Optional: Set playback config
+    AudioPro.configure({ contentType: AudioProContentType.MUSIC, debug: __DEV__ });
+
+    // Define an audio track (supports static remote files, live streams, and local files)
+    const track = {
+      id: 'remote-song-1',
+      url: 'https://rnap.dev/audio-soundhelix-song-1-tschurger.mp3',
+      title: 'Soundhelix Song 1',
+      artwork: 'https://rnap.dev/artwork-usgs-bAji8qv_LlY-unsplash.jpg',
+      artist: 'T. Schurger',
+    };
+  
+    // Load and play the track
+    AudioPro.play(track);
+
+    
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
---
+++ components/HapticTab.tsx
@@ -0,0 +1,9 @@
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import * as Haptics from 'expo-haptics';
+
+export function HapticTab(props: BottomTabBarButtonProps) {
+  return (
+   <>
+   </>
+  );
+}
